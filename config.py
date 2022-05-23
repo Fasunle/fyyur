5c4eb1c021e2a25fc64d53fd1c123e2c0c62b0c3
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-23 13:50:23 +0100

    Config: set database URI string

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# ? IMPLEMENTED DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://kehinde@localhost:5432/fyyurapp'
