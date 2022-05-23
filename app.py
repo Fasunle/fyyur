12de3f0a54ecea4d55cc612f35a712765078ab2e
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-23 22:56:28 +0100

    Update Artist Schema

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 
+from email.policy import default
 import json
 import dateutil.parser
 import babel
@@ -49,21 +50,17 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
     facebook_link = db.Column(db.String(120))
-    address = db.Column(db.String(120), nullable=False)
-    website = db.Column(db.String(120), nullable=False)
-    seeking_decription = db.Column(db.String(120), nullable=False)
-    seeking_talent = db.Column(db.Boolean, default=True, nullable=False)
-    past_shows_count = db.Column(db.Integer, default=0, nullable=False)
-    upcoming_shows_count = db.Column(db.Integer, default=0, nullable=False)
+    website_link = db.Column(db.String(120), nullable=False)
+    seeking_venue = db.Column(db.Boolean, default=False, nullable=False)
+    seeking_description = db.Column(db.String(120), nullable=False)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
