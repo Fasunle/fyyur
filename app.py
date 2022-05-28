c862250bf103626265d53465a61366241936685a
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-28 13:37:47 +0100

    User can search for both venue or artist
    TODO: case-insensitive search

--- app.py
+++ app.py
@@ -169,18 +169,28 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for Hop should return "The Musical Hop".
+    #  implement search on artists with partial string search. Ensure it is
+    # TODO: case-insensitive.
+    #  seach for Hop should return "The Musical Hop".
     # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
-    }
-    return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    search_term = str(request.form.get('search_term', '')).lower()
+    found_venues = Venue.query.filter(Venue.name.op("~")(search_term)).all()
+    shows = Show.query.all()
+    upcoming_shows = past_or_upcoming_shows(shows)["upcoming"]
+    response = {"data": [], "count": len(found_venues)}
+
+    for venue in found_venues:
+        response["data"].append(
+            {
+                "id": venue.id,
+                "name": venue.name,
+                "num_upcomint_shows": len(
+                    [upcoming_show.venue_id is venue.id for upcoming_show in upcoming_shows]
+                )
+            }
+        )
+
+    return render_template('pages/search_venues.html', results=response, search_term=search_term)
 
 
 @app.route('/venues/<int:venue_id>')
@@ -355,18 +365,29 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    # implement search on artists with partial string search. Ensure it is
+    # TODO: case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+
+    search_term = str(request.form.get('search_term', '')).lower()
+    found_artists = Artist.query.filter(Artist.name.op("~")(search_term)).all()
+    shows = Show.query.all()
+    upcoming_shows = past_or_upcoming_shows(shows)["upcoming"]
+    response = {"data": [], "count": len(found_artists)}
+
+    for artist in found_artists:
+        response["data"].append(
+            {
+                "id": artist.id,
+                "name": artist.name,
+                "num_upcomint_shows": len(
+                    [upcoming_show.artist_id is artist.id for upcoming_show in upcoming_shows]
+                )
+            }
+        )
+
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
