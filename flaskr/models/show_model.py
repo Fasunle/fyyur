c6acec00f2ae8c8599d3ce2302622f221a4a0882
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-02 13:53:29 +0100

    Show schema model without the methods

---
+++ flaskr/models/show_model.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from flaskr.models import db
+
+
+class Show(db.Model):
+    """This links both the Artist Model and Venue model via their primary keys. This, it has a relatioship with the both.
+
+    Args:
+        db (Model): Base class which enables us to use SQLAlchemy API on our class 'Show'
+    """
+
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(
+        db.Integer, db.ForeignKey("Artist.id"), nullable=False)
+    venue_id = db.Column(
+        db.Integer, db.ForeignKey("Venue.id"), nullable=False)
+    start_time = db.Column(db.DateTime, default=datetime.now())
