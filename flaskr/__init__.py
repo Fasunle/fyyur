b1223c756380b4d21dd8b93a88fe6771dbd98f7b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-02 12:57:24 +0100

    Create app - entrypoint to the fyyur app

---
+++ flaskr/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+from flaskr.controllers import controllers
+from flaskr.models import setup_db
+from flaskr.utils import format_datetime
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # CONFIGURATIONS
+    app.config.from_pyfile("config.py")
+    app.jinja_env.filters['datetime'] = format_datetime
+    setup_db(app)
+    # controllers
+    controllers(app)
+
+    return app
